b3199dccdabd9cbdbb53e0ba361cc543c5dab11d
Victor Capilla <victorcapilladeveloper@gmail.com>
2024-08-16 16:42:56 +0200

    Setup connection with Mongo from Backend

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
-
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
+const Person = require('./models/person')
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
@@ -38,36 +17,39 @@ app.use(express.json())
 app.use(requestLogger)
 app.use(morgan(function (tokens, req, res) {
     return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      tokens.content(req, res)
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        tokens.content(req, res)
     ].join(' ')
-  }))
+}))
 // app.use(morgan('tiny'))
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/info', (request, response) => {
-    response.send(
-        `<p>Phonebook has info for ${persons.length} people</p>
-        <p>${Date()}</p>`
-    )
+    Person.find({}).then(persons => {
+        response.send(
+            `<p>Phonebook has info for ${persons.length} people</p>
+            <p>${Date()}</p>`
+        )
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Person.find({ _id: `${request.params.id}`}).then(persons => {
+        if (persons) {
+            response.json(persons)
+        } else {
+            response.status(404).end()
+        }
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -111,9 +93,9 @@ app.post('/api/persons', (request, response) => {
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
-  app.use(unknownEndpoint)
+}
+
+app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- mongo.js
+++ mongo.js
@@ -1,44 +1,44 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
-}
+// if (process.argv.length < 3) {
+//     console.log('give password as argument')
+//     process.exit(1)
+// }
 
-const password = process.argv[2]
-const name = process.argv[3]
-const number = process.argv[4]
+// const password = process.argv[2]
+// const name = process.argv[3]
+// const number = process.argv[4]
 
-const url =
-    `mongodb+srv://victorcapilladeveloper:${password}@cluster0.ekukp.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+// const url =
+//     `mongodb+srv://victorcapilladeveloper:${password}@cluster0.ekukp.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery', false)
+// mongoose.set('strictQuery', false)
 
-mongoose.connect(url)
+// mongoose.connect(url)
 
-const personsSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-})
+// const personsSchema = new mongoose.Schema({
+//     name: String,
+//     number: String,
+// })
 
-const Person = mongoose.model('Person', personsSchema)
+// const Person = mongoose.model('Person', personsSchema)
 
-if (name != null && number != null) {
-    const person = new Person({
-        name: `${name}`,
-        number: `${number}`,
-    })
+// if (name != null && number != null) {
+//     const person = new Person({
+//         name: `${name}`,
+//         number: `${number}`,
+//     })
 
-    person.save().then(result => {
-        console.log(`added ${name} number ${number} to phonebook`)
-        mongoose.connection.close()
-    })
-} else {
-    Person.find({}).then(result => {
-        console.log("phonebook:")
-        result.forEach(person => {
-            console.log(person.name, person.number)
-        })
-        mongoose.connection.close()
-    })
-}
+//     person.save().then(result => {
+//         console.log(`added ${name} number ${number} to phonebook`)
+//         mongoose.connection.close()
+//     })
+// } else {
+//     Person.find({}).then(result => {
+//         console.log("phonebook:")
+//         result.forEach(person => {
+//             console.log(person.name, person.number)
+//         })
+//         mongoose.connection.close()
+//     })
+// }
